6c76f02c95c46775d38f45a49878d523f239f8d6
吕强 <lq42478@ly.com>
2017-11-21 13:15:41 +0800

    因为一个小失误，差点怀疑人生

--- register-center/src/main/java/me/rainbowland/coffee/register/RegisterCenterApplication.java
+++ register-center/src/main/java/me/rainbowland/coffee/register/RegisterCenterApplication.java
@@ -2,7 +2,6 @@ package me.rainbowland.coffee.register;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -10,7 +9,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
-@EnableDiscoveryClient
 public class RegisterCenterApplication {
 
 	public static void main(String[] args) {
