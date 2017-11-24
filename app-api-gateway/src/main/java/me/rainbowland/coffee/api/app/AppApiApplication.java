acb5116ac2dc0eb1a5b401d06bc2f143444381eb
吕强 <lq42478@ly.com>
2017-11-24 15:38:13 +0800

    加入app api网关

---
+++ app-api-gateway/src/main/java/me/rainbowland/coffee/api/app/AppApiApplication.java
@@ -0,0 +1,17 @@
+package me.rainbowland.coffee.api.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Created by lvqiang on 2017/11/24.
+ */
+@EnableZuulProxy
+@SpringCloudApplication
+public class AppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApiApplication.class, args);
+	}
+}
