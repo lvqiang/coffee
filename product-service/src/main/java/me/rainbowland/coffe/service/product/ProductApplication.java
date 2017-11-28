973684f1d5d98acc8322f8c74faf96d3d2fe60e2
吕强 <lq42478@ly.com>
2017-11-28 11:05:19 +0800

    fix bug

--- product-service/src/main/java/me/rainbowland/coffe/service/product/ProductApplication.java
+++ product-service/src/main/java/me/rainbowland/coffe/service/product/ProductApplication.java
@@ -2,11 +2,15 @@ package me.rainbowland.coffe.service.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 /**
  * Created by lvqiang on 2017/11/21.
  */
+@EnableDiscoveryClient
 @SpringBootApplication
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ProductApplication {
 
 	public static void main(String[] args) {
